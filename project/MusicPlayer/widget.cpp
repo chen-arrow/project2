175b640c4fa1ad25172a804bb75271a44fda78d8
pppyc6 <1228957637@qq.com>
2023-04-03 22:44:42 +0800

    合并唱片动画

--- project/MusicPlayer/widget.cpp
+++ project/MusicPlayer/widget.cpp
@@ -58,6 +58,14 @@ Widget::Widget(QWidget *parent)
         }
     });
 
+    m_pTimer=new QTimer();
+    m_pTimer->setInterval(25);
+        connect(m_pTimer,&QTimer::timeout,this,&Widget::upDateRote);
+
+    s_pTimer=new QTimer(this);
+    s_pTimer->setInterval(15);
+    connect(s_pTimer,&QTimer::timeout,this,&Widget::upDateStylusIn);
+
     //按钮移动，还原
     connect(ui->pushButton,&QPushButton::pressed,this,&Widget::btnmove);
     connect(ui->pushButton,&QPushButton::released,this,&Widget::btnmoveback);
@@ -131,10 +139,14 @@ void Widget::on_pushButton_3_clicked()
         case QMediaPlayer::PlaybackState::PlayingState: //播放状态
             //如果现在正在播放就暂停音乐
             mediaPlayer->pause();
+            m_pTimer->stop();
+            upDateStylusOut();
             ui->pushButton_3->setStyleSheet("border-image: url(:/picture/play.png);");
             break;
         case QMediaPlayer::PlaybackState::PausedState: //暂停状态
              //如果现在是暂停就继续播放
+            m_pTimer->start(100);
+            upDateStylusIn();
             ui->pushButton_3->setStyleSheet("border-image: url(:/picture/playing.png);");
             mediaPlayer->play();
             break;
@@ -215,4 +227,103 @@ void Widget::btnmoveback()
     btn->move(btn->geometry().x() - 1, btn->geometry().y() - 1);
 }
 
+void Widget::paintEvent(QPaintEvent *event)
+{
+    QPainter painter(this);
+    painter.setRenderHint(QPainter::SmoothPixmapTransform);
+    painter.setRenderHint(QPainter::Antialiasing);
+//    painter.setPen(QPen(QColor(0, 0, 0, 150)));
+//    QPainterPath painterPath;
+//    painterPath.addRect(40,40,300,300);
+////    painterPath.addEllipse(80,80,220,220);
+//    painter.setClipPath(painterPath);
+
+//    painter.drawPath(painterPath);
+
+
+    painter.drawPixmap(rect(),QPixmap(":/picture/background1.png"),QRect());
+
+    QPixmap pixMap3(":/picture/cdCover");
+    painter.save();
+    painter.translate(85/1.5,110/1.5);
+    painter.translate(195/1.5,195/1.5);
+    painter.rotate(2*cd_Rote);
+    painter.translate(-75/1.5,-75/1.5);
+    painter.drawPixmap(0,0,150/1.5,150/1.5,pixMap3);
+    painter.restore();
+
+    QPixmap pixMap1(":/picture/record");
+    painter.save();
+    painter.translate(85/1.5,110/1.5);//move to rect left_top
+    painter.translate(195/1.5,195/1.5);
+    painter.rotate(2*cd_Rote);
+    painter.translate(-195/1.5,-195/1.5);
+    painter.drawPixmap(0,0,390/1.5,390/1.5,pixMap1);
+    painter.restore();
+
+    QPixmap pixMap2(":/picture/stylus");
+
+    painter.save();
+    painter.translate(30/1.5,-200/1.5);
+    painter.translate(250/1.5,250/1.5);
+    painter.rotate(s_Rote);
+    painter.translate(-250/1.5,-250/1.5);
+    painter.drawPixmap(0,0,500/1.5,500/1.5,pixMap2);
+    painter.restore();
+
+}
+
+void Widget::upDateRote()
+{
+    cd_Rote=cd_Rote+1;
+    if(cd_Rote==180)
+    {
+        cd_Rote=0;
+    }
+    update();
+}
+
+void Widget::onStartorStopRote()
+{
+    m_bStart=!m_bStart;
+    if(m_bStart)
+    {
+        m_pTimer->start(100);
+        s_pTimer->start(300);
+    }
+    else
+    {
+        m_pTimer->stop();
+        s_pTimer->stop();
+    }
+}
+void Widget::upDateStylusIn()
+{
+    s_pTimer->start(10);
+    s_Rote += 1;
+    if(s_Rote > 30)
+    {
+        s_pTimer->stop();
+    }
+    update();
+}
+void Widget::upDateStylusOut()
+{
+    s_Rote =0;
+
+    update();
+}
+
+void Widget::on_pushButton_Theme_clicked()
+{
+
+    if(iftheme==0){
+        LoadQss::loadQss(":/qss/style.qss");
+        iftheme++;
+    }
+    else{
+        LoadQss::loadQss(":/qss/origin.qss");
+        iftheme--;
+    }
+}
 
--- project/MusicPlayer/widget.h
+++ project/MusicPlayer/widget.h
@@ -1,6 +1,22 @@
 #ifndef WIDGET_H
 #define WIDGET_H
+#ifndef LOADQSS_H
+#define LOADQSS_H
 
+#include <QFile>
+#include <QApplication>
+#include <QWidget>
+#include <QUrl>
+#include <QMediaPlayer>
+#include <QAudioOutput>
+#include <QWidget>
+#include <QPixmap>
+#include <QTimer>
+#include <QLabel>
+#include <QPushButton>
+#include <QPaintEvent>
+#include <QPainter>
+#include <QPropertyAnimation>
 #include <QWidget>
 #include <QUrl>
 #include <QMediaPlayer>
@@ -10,13 +26,32 @@ QT_BEGIN_NAMESPACE
 namespace Ui { class Widget; }
 QT_END_NAMESPACE
 
+
+
 class Widget : public QWidget
 {
     Q_OBJECT
 
+class LoadQss
+{
+public:
+    static void loadQss(const QString &path)
+    {
+        QFile qss(path);
+        qss.open(QFile::ReadOnly);
+        qApp->setStyleSheet(qss.readAll());
+        qss.close();
+    }
+};
+
+#endif // LOADQSS_H
+
 public:
     Widget(QWidget *parent = nullptr);
     ~Widget();
+
+protected:
+    void paintEvent(QPaintEvent *event);
 
 private slots:
 
@@ -40,7 +75,15 @@ private slots:
 
     void btnmoveback();
 
+    void upDateRote();
 
+    void onStartorStopRote();
+
+    void upDateStylusIn();
+
+    void upDateStylusOut();
+
+    void on_pushButton_Theme_clicked();
 private:
     Ui::Widget *ui;
     QList<QUrl> playList;  //播放列表
@@ -49,6 +92,15 @@ private:
     int curPlayIndex = 0;
 
     int ifplay = 0; // 暂停播放按钮
+    int iftheme=0;
+    QTimer *m_pTimer;
+    QTimer *s_pTimer;
+    int cd_Rote=0;
+    int s_Rote=0;
+
+    QPushButton *m_pbtn=nullptr;
+    bool m_bStart=false;
 
 };
 #endif // WIDGET_H
+
